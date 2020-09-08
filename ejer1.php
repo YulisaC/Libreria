2acffb6797687138ed7e619c759f279acbf08e76
YulisaC <34174870+YulisaC@users.noreply.github.com>
2020-09-08 15:00:23 -0500

    Add files via upload

---
+++ ejer1.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title> Progrmamción en PHP </title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<?php
+			$mensaje1="Bienvenidos";
+			$mensaje2="al mundo de la programación";
+			$mensaje3="de paginas en PHP";
+			$mensajecompleto=$mensaje1.$mensaje2.$mensaje3;
+			echo $mensajecompleto;
+		 ?>
+	</body>
+</html>
